8144ea0eeba995f7415cf037d506581699e10009
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-10-17 23:16:28 +0545

    Update infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -8,6 +8,7 @@ repo init -u https://github.com/ProjectInfinity-X/manifest -b QPR3 --git-lfs --d
 
 # Synchronize the repo using a crave resync script
 /opt/crave/resync.sh
+/opt/crave/resync.sh
 
 # Remove unneeded or outdated directories for clean-up before the next steps
 rm -rf hardware/qcom-caf/sm8150/media
